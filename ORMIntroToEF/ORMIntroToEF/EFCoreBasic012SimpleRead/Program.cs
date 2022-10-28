77bd77981806f02a39ab9d835056d7c1d8cc1261
stdg3 <velikoglosv@mail.ru>
2022-10-28 18:58:31 +0300

    simple read in ef core

---
+++ ORMIntroToEF/ORMIntroToEF/EFCoreBasic012SimpleRead/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+CreateEmptyDb();
+AddData();
+ReadData();
+
+
+static void CreateEmptyDb()
+{
+    using var dbContext = new ApplicationDbContext();
+    dbContext.Database.EnsureDeleted();
+    dbContext.Database.EnsureCreated();
+}
+
+static void AddData()
+{
+    using var dbContext = new ApplicationDbContext();
+
+    var csharpCourse = new Course
+    {
+        Name = "CSharp",
+        LessonsQuantity = 7
+    };
+
+    var efCore = new Course
+    {
+        Name = "EF Core",
+        LessonsQuantity = 10
+    };
+
+    dbContext.Add(csharpCourse);
+    dbContext.Add(efCore);
+    dbContext.SaveChanges();
+}
+
+static void ReadData()
+{
+    using var dbContext = new ApplicationDbContext();
+
+    // get all courses
+    var courses = dbContext.Courses.ToList();
+
+    Console.WriteLine(new String('-', 80));
+    foreach (var course in courses)
+    {
+        Console.WriteLine($"Course Name: {course.Name} "+
+            $"Qt: {course.LessonsQuantity}");
+    }
+    Console.WriteLine(new String('-', 80));
+    var generatedSql = dbContext.Courses.ToQueryString();
+    Console.WriteLine($"Genearted Sql: {generatedSql}.");
+}
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Course> Courses { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EfCoreBasicDb;Trusted_Connection=True;");
+    }
+}
+
+public class Course 
+{
+    [Key]
+    public int CourseId { get; set; }
+    public string Name { get; set; }
+    public int LessonsQuantity { get; set; }
+}
