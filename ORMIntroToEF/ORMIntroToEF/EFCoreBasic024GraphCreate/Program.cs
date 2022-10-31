bb610bdce2af696f79d8ae3388cdb1746bdd5ac7
stdg3 <velikoglosv@mail.ru>
2022-10-31 11:43:44 +0200

    create graph releation

---
+++ ORMIntroToEF/ORMIntroToEF/EFCoreBasic024GraphCreate/Program.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Globalization;
+
+CreateEntity();
+AddData();
+ReadData();
+
+static void CreateEntity()
+{
+    using var dbContext = new ApplicationDbContext();
+    dbContext.Database.EnsureDeleted();
+    dbContext.Database.EnsureCreated();
+}
+
+static void AddData()
+{
+    using var context = new ApplicationDbContext();
+
+    var cSharp = new Course
+    {
+        Name = "CSharp",
+        LessonQuantity = 5
+    };
+
+    var efCore = new Course
+    {
+        Name = "EF Core",
+        LessonQuantity = 3
+    };
+
+    var john = new Author
+    {
+        LastName = "Smith",
+        FirstName = "John"
+    };
+
+    var arthur = new Author
+    {
+        FirstName = "Arthur",
+        LastName = "Morgan"
+    };
+
+    cSharp.Author = john;
+    efCore.Author = john;
+
+    context.Add(arthur);
+    context.Add(cSharp);
+    context.Add(efCore);
+    context.SaveChanges();
+}
+
+static void ReadData()
+{
+    using var dbContext = new ApplicationDbContext();
+
+    var courses = dbContext
+        .Courses
+        .Include(c => c.Author)
+        .ToList();
+
+    foreach (var course in courses)
+    {
+        Console.WriteLine($"Course Name: {course.Name}. Qt: {course.LessonQuantity}."+
+            $"Author: {course.Author.FirstName + " " + course.Author.LastName}");
+    }
+
+    var authors = dbContext.Authors.ToList();
+
+    foreach (var author in authors)
+    {
+        Console.WriteLine($"Author: {author.FirstName + " " + author.LastName}");
+    }
+}
+
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Author> Authors { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EfCoreBasicDb;Trusted_Connection=True;")
+            .EnableDetailedErrors()
+            .EnableSensitiveDataLogging()
+            .LogTo(
+        Console.WriteLine,
+        LogLevel.Information);
+    }
+}
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int LessonQuantity { get; set; }
+    public Author Author { get; set; }
+}
+
+public class Author
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName{ get; set; }
+    public ICollection<Course> Courses { get; set; }
+}
